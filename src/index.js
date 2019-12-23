e7540a005ec5eec066f0383cb001f07ec50e50d9
morpheus1729 <55521972+morpheus1729@users.noreply.github.com>
2019-12-23 20:36:11 +0530

    firstComitt
    Chaning the name

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
+<h1>Hello !</h1>
 <div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+  This is Sagar and this is my first Repo
 </div>
 `;
